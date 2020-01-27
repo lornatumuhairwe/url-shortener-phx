29a96466635cca8a003eb00399b1e8efd7704537
Lorna Tumuhairwe <lornatumuhairwe@gmail.com>
2020-01-27 14:40:29 +0300

    add test for show route.

--- lib/tnyurl_web/controllers/home_controller.ex
+++ lib/tnyurl_web/controllers/home_controller.ex
@@ -2,6 +2,8 @@ defmodule TnyurlWeb.HomeController do
   use TnyurlWeb, :controller
 
   alias Tnyurl.Urls
+  alias Tnyurl.Urls.Url
+  alias TnyurlWeb.ErrorView
 
   def index(conn, params) do
     changeset = Urls.change_url(params)
@@ -11,7 +13,7 @@ defmodule TnyurlWeb.HomeController do
 
   def create(conn, %{"url" => url}) do
     case Urls.create_url(url) do
-      {:ok, url} ->
+      {:ok, _} ->
         conn
         |> put_flash(:info, "Success!")
         |> redirect(to: "/")
@@ -23,6 +25,14 @@ defmodule TnyurlWeb.HomeController do
 
   def show(conn, %{"slug" => slug}) do
     url = Urls.get_url(slug)
-    redirect(conn, external: url.name)
+    case url do
+      %Url{} ->
+        redirect(conn, external: url.name)
+      _ ->
+        conn
+        |> put_status(:not_found)
+        |> put_view(ErrorView)
+        |> render(:"404")
+    end
   end
 end
--- test/tnyurl_web/controllers/home_controller_test.exs
+++ test/tnyurl_web/controllers/home_controller_test.exs
@@ -1,5 +1,8 @@
 defmodule TnyurlWeb.HomeControllerTest do
   use TnyurlWeb.ConnCase
+  alias Tnyurl.Urls
+
+  @valid_attributes %{name: "http://youtube.com"}
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
@@ -7,7 +10,7 @@ defmodule TnyurlWeb.HomeControllerTest do
   end
 
   test "POST /shorten", %{conn: conn} do
-    conn = post(conn, "/shorten", url: %{name: "http://youtube.com"})
+    conn = post(conn, "/shorten", url: @valid_attributes)
     assert html_response(conn, 302)
   end
 
@@ -19,5 +22,16 @@ defmodule TnyurlWeb.HomeControllerTest do
   test "POST /shorten with a name that is not a url", %{conn: conn} do
     conn = post(conn, "/shorten", url: %{name: "url that is not a url"})
     assert html_response(conn, 200) =~ "has invalid format"
+  end
+
+  test "GET /:slug without an existing slug returns 404", %{conn: conn} do
+    conn = get(conn, "/non-existing-slug")
+    assert html_response(conn, 404)
+  end
+
+  test "GET /:slug with an existing slug redirects", %{conn: conn} do
+    {:ok, url} = Urls.create_url(@valid_attributes)
+    conn = get(conn, "/" <> url.slug)
+    assert html_response(conn, 302)
   end
 end
